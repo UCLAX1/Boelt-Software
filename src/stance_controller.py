66031db3dd0dca2659e3cd1f690905498c0bb785
emilyliang3 <59712728+emilyliang3@users.noreply.github.com>
2023-04-21 16:07:42 -0700

    Updated StanceController- removed rotation and movement in z-dir

--- src/stance_controller.py
+++ src/stance_controller.py
@@ -1,48 +1,31 @@
 import numpy as np
-from transforms3d.euler import euler2mat
 
 class StanceController:
     def __init__(self, config):
         self.config = config
 
-
     def position_delta(self, leg_index, state, command):
-        """Calculate the difference between the next desired body location and the current body location
-        
-        Parameters
-        ----------
-        z_measured : float
-            Z coordinate of the feet relative to the body.
-        stance_params : StanceParams
-            Stance parameters object.
-        movement_reference : MovementReference
-            Movement reference object.
-        gait_params : GaitParams
-            Gait parameters object.
-
-        Returns
-        -------
-        (Numpy array (3), Numpy array (3, 3))
-            (Position increment, rotation matrix increment)
         """
-        z = state.foot_locations[2, leg_index]
-        v_xy = np.array(
+        Calculate the difference between the next desired body location and the current body location
+        Returns position increment 
+        """
+        z = state.foot_locations[2, leg_index] # z coord of foot relative to body
+        v_xy = np.array( 
             [
                 -command.horizontal_velocity[0],
                 -command.horizontal_velocity[1],
-                1.0
-                / self.config.z_time_constant
-                * (state.height - z),
+                0 # zero bc boelt not moving up and down; maybe change later
             ]
         )
-        delta_p = v_xy * self.config.dt
-        delta_R = euler2mat(0, 0, -command.yaw_rate * self.config.dt)
-        return (delta_p, delta_R)
+        delta_p = v_xy * self.config.dt # distance = velocity * time 
+        return delta_p
 
-    # TODO: put current foot location into state
     def next_foot_location(self, leg_index, state, command):
-        foot_location = state.foot_locations[:, leg_index]
-        (delta_p, delta_R) = self.position_delta(leg_index, state, command)
-        incremented_location = delta_R @ foot_location + delta_p
+        """
+        Returns new position
+        """
+        foot_location = state.foot_locations[:, leg_index] # xyz of foot pos.
+        delta_p = self.position_delta(leg_index, state, command)
+        incremented_location = foot_location + delta_p
 
         return incremented_location
