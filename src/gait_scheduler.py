d972302c1778cf086fe0be5dff6d72fe103b9049
sabuhamad <sari.abuhamad@gmail.com>
2023-04-21 15:14:17 -0700

    Updated state at 0 or -1

--- src/gait_scheduler.py
+++ src/gait_scheduler.py
@@ -1,6 +1,5 @@
 #The gait scheduler is responsible for planning which feet should be on the ground (stance) 
 #and which should be moving forward to the next step (swing) at any given time. 
-#In a trot for example, the diagonal pairs of legs move in sync and take turns between stance and swing
 
 class GaitScheduler:
     def __init__(self, config):
@@ -9,8 +8,9 @@ class GaitScheduler:
     def whichFeetOnGround(self, state):
         if(state == 0 or state == -1):
             self.nextStance = [1,1,1,1]
+            self.prevStance = [1,1,1,1]
         elif(state == 1):
-            if(self.prevStance == [1,1,0,1]):
+            if(self.prevStance == [1,1,0,1]): 
                 self.nextStance = [1,0,0,1]
                 self.prevStance = [1,0,0,1]
             elif(self.prevStance == [1,0,0,1]):
@@ -30,3 +30,4 @@ class GaitScheduler:
                 self.prevStance = [0,1,0,1]
             else:
                 self.nextStance = [1,1,0,1]
+                self.prevStance = [1,1,0,1]
