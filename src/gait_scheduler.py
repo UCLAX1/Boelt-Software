98c369e21ab4d4b98cb5c61c6a17d38d4cf87e3c
sabuhamad <sari.abuhamad@gmail.com>
2023-04-21 15:26:58 -0700

    Updated Walk stances to have a period of pi/2 in between each stance

--- src/gait_scheduler.py
+++ src/gait_scheduler.py
@@ -6,28 +6,19 @@ class GaitScheduler:
         self.config = config
     
     def whichFeetOnGround(self, state):
-        if(state == 0 or state == -1):
+        if(state == 0 or state == -1): #Rest or Deactivated
             self.nextStance = [1,1,1,1]
             self.prevStance = [1,1,1,1]
-        elif(state == 1):
-            if(self.prevStance == [1,1,0,1]): 
-                self.nextStance = [1,0,0,1]
-                self.prevStance = [1,0,0,1]
-            elif(self.prevStance == [1,0,0,1]):
-                self.nextStance = [1,0,1,1]
-                self.prevStance = [1,0,1,1]
-            elif(self.prevStance == [1,0,1,1]):
-                self.nextStance = [1,0,1,0]
-                self.prevStance = [1,0,1,0]
-            elif(self.prevStance == [1,0,1,0]):
-                self.nextStance = [0,1,1,0]
-                self.prevStance = [0,1,1,0]
-            elif(self.prevStance == [0,1,1,0]):
+        elif(state == 1): #Walk -> pi/2 between each state
+            if(self.prevStance == [1,0,1,1]): 
+                self.nextStance = [1,1,1,0]
+                self.prevStance = [1,1,1,0]
+            elif(self.prevStance == [1,1,1,0]):
                 self.nextStance = [0,1,1,1]
                 self.prevStance = [0,1,1,1]
             elif(self.prevStance == [0,1,1,1]):
-                self.nextStance = [0,1,0,1]
-                self.prevStance = [0,1,0,1]
-            else:
                 self.nextStance = [1,1,0,1]
                 self.prevStance = [1,1,0,1]
+            else:
+                self.nextStance = [1,0,1,1]
+                self.prevStance = [1,0,1,1]
